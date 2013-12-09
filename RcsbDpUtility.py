fc71d4178767700913520dda935af5e5bcf88f15
John Westbrook <jdwestbrook@gmail.com>
2013-12-09 17:23:42 +0000

    Checkpoint update

--- RcsbDpUtility.py
+++ RcsbDpUtility.py
@@ -113,7 +113,7 @@ class RcsbDpUtility(object):
         self.__annotationOps = ["annot-secondary-structure", "annot-link-ssbond", "annot-cis-peptide","annot-distant-solvent",
                                 "annot-merge-struct-site","annot-reposition-solvent","annot-base-pair-info",
                                 "annot-validation","annot-site","annot-rcsb2pdbx","annot-consolidated-tasks",
-                                "annot-wwpdb-validate-1","annot-wwpdb-validate-2","prd-search",
+                                "annot-wwpdb-validate-test","annot-wwpdb-validate-2","prd-search",
                                 "annot-chem-shift-check","annot-chem-shift-coord-check","annot-nmrstar2pdbx","annot-pdbx2nmrstar",
                                 "annot-reposition-solvent-add-derived", "annot-rcsb2pdbx-strip", "annot-rcsbeps2pdbx-strip",
                                 "annot-rcsb2pdbx-strip-plus-entity", "annot-rcsbeps2pdbx-strip-plus-entity",
@@ -760,14 +760,15 @@ class RcsbDpUtility(object):
             else:
                 cmd += " ; cp -f  " + iPath + " " + oPath                
 
-        elif (op == "annot-wwpdb-validate-1"):
+        elif (op == "annot-wwpdb-validate-2"):
+            # For the second version of the validation package --
             #
             # The validation package is currently set to self configure in a wrapper
             # shell script.  No environment settings are required here at this point.
             #
             cmd += " ; WWPDB_SITE_ID="  + self.__siteId  + " ; export WWPDB_SITE_ID "
             cmd += " ; DEPLOY_DIR="  + self.__deployPath  + " ; export DEPLOY_DIR "
-            cmdPath =os.path.join(self.__packagePath,"Vpack","scripts","vpack-run.sh")
+            cmdPath =os.path.join(self.__packagePath,"Vpack-v2","scripts","vpack-run.sh")
             thisCmd  = " ; " + cmdPath                        
             
             if  self.__inputParamDict.has_key('sf_file_path'):
@@ -788,9 +789,8 @@ class RcsbDpUtility(object):
             cmd += thisCmd + " 1abc " + iPathFull + " " + sfPathFull + " " + pdfPath +  " " + xmlPath + " " + cleanOpt 
             cmd += " > " + tPath + " 2>&1 ; cat " + tPath + " >> " + lPath
             cmd += " ; cp  -f " + pdfPath + " " + oPath
-            #
-            #
-        elif (op == "annot-wwpdb-validate-2"):
+
+        elif (op == "annot-wwpdb-validate-test"):
             # For the second version of the validation package --
             #
             # The validation package is currently set to self configure in a wrapper
@@ -798,7 +798,7 @@ class RcsbDpUtility(object):
             #
             cmd += " ; WWPDB_SITE_ID="  + self.__siteId  + " ; export WWPDB_SITE_ID "
             cmd += " ; DEPLOY_DIR="  + self.__deployPath  + " ; export DEPLOY_DIR "
-            cmdPath =os.path.join(self.__packagePath,"Vpack-v2","scripts","vpack-run.sh")
+            cmdPath =os.path.join(self.__packagePath,"Vpack-test","scripts","vpack-run.sh")
             thisCmd  = " ; " + cmdPath                        
             
             if  self.__inputParamDict.has_key('sf_file_path'):
