db227b49003d6a0b48fad2fea3be74c545867a51
Ezra Peisach <ezra.peisach@rcsb.org>
2020-10-28 06:04:53 -0400

    0.28.dev3

--- wwpdb/utils/dp/__init__.py
+++ wwpdb/utils/dp/__init__.py
@@ -3,5 +3,5 @@ __docformat__ = "restructuredtext en"
 __author__ = "John Westbrook"
 __email__ = "john.westbrook@rcsb.org"
 __license__ = "Apache 2.0"
-__version__ = "0.28.dev2"
+__version__ = "0.28.dev3"
 
