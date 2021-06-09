82e8f3abbf4dac025ac135155af0569c0c65ac97
jmb <jmb@ebi.ac.uk>
2021-06-09 09:06:44 +0100

    clean up added to tasks auto clean up code allowed siteid to be passed through

--- wwpdb/utils/dp/DensityWrapper.py
+++ wwpdb/utils/dp/DensityWrapper.py
@@ -1,25 +1,18 @@
-import os
-import sys
-import logging
 import argparse
-import tempfile
+import logging
+import os
 import shutil
+import sys
+import tempfile
 
 from wwpdb.utils.config.ConfigInfo import getSiteId
-from wwpdb.utils.dp.RcsbDpUtility import RcsbDpUtility
 
-# from wwpdb.utils.config.ConfigInfoApp import ConfigInfoAppCommon
-# from wwpdb.utils.dp.electron_density.x_ray_density_map import XrayVolumeServerMap
-# from wwpdb.utils.dp.electron_density.em_density_map import EmVolumes
+from wwpdb.utils.dp.RcsbDpUtility import RcsbDpUtility
 
 
 class DensityWrapper(object):
-    def __init__(self):
-        self.__site_id = getSiteId()
-        # self.__cICommon = ConfigInfoAppCommon(self.__site_id)
-        # self.node_path = self.__cICommon.get_node_bin_path()
-        # self.volume_server_pack = self.__cICommon.get_volume_server_pack_path()
-        # self.volume_server_query = self.__cICommon.get_volume_server_query_path()
+    def __init__(self, site_id=None):
+        self.__site_id = site_id if site_id else getSiteId()
 
     def convert_xray_density_map(self, coord_file, in_2fofc_cif, in_fofc_cif, out_binary_volume, working_dir):
 
@@ -30,21 +23,12 @@ class DensityWrapper(object):
         rdb.addInput(name='one_fo_fc_cif', value=in_fofc_cif)
         rdb.op('xray-density-bcif')
         rdb.exp(out_binary_volume)
+        rdb.cleanup()
 
         if out_binary_volume:
             if os.path.exists(out_binary_volume):
                 return True
         return False
-
-        # xray_conversion = XrayVolumeServerMap(coord_path=coord_file,
-        #                                      binary_map_out=out_binary_cif,
-        #                                      node_path=self.node_path,
-        #                                      volume_server_query_path=self.volume_server_query,
-        #                                      volume_server_pack_path=self.volume_server_pack,
-        #                                      two_fofc_mmcif_map_coeff_in=in_2fofc_map,
-        #                                      fofc_mmcif_map_coeff_in=in_fofc_map,
-        #                                      working_dir=working_dir)
-        # return xray_conversion.run_process()
 
     def convert_em_volume(self, in_em_volume, out_binary_volume, working_dir):
 
@@ -53,19 +37,12 @@ class DensityWrapper(object):
         rdb.imp(in_em_volume)
         rdb.op('em-density-bcif')
         rdb.exp(out_binary_volume)
+        rdb.cleanup()
 
         if out_binary_volume:
             if os.path.exists(out_binary_volume):
                 return True
         return False
-
-        # em_conversion = EmVolumes(em_map=in_em_volume,
-        #                          binary_map_out=out_binary_volume,
-        #                          node_path=self.node_path,
-        #                          volume_server_pack_path=self.volume_server_pack,
-        #                          volume_server_query_path=self.volume_server_query,
-        #                          working_dir=working_dir)
-        # return em_conversion.run_conversion()
 
 
 def main():
