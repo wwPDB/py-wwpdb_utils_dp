251205dc28779c9e8e6d4832402d18a7390370be
Ezra Peisach <ezra.peisach@rcsb.org>
2024-04-27 21:51:42 -0400

    Complexity tests - and add in environment variable context - to find install while running

--- wwpdb/utils/tests-dp/commonsetup.py
+++ wwpdb/utils/tests-dp/commonsetup.py
@@ -2,6 +2,7 @@
 
 import os
 import platform
+import contextlib
 
 HERE = os.path.abspath(os.path.dirname(__file__))
 TOPDIR = os.path.dirname(os.path.dirname(os.path.dirname(HERE)))
@@ -40,3 +41,35 @@ else:
 class commonsetup(object):
     def __init__(self):
         pass
+
+
+# From https://stackoverflow.com/questions/2059482/temporarily-modify-the-current-processs-environment
+@contextlib.contextmanager
+def modified_environ(*remove, **update):
+    """
+    Temporarily updates the ``os.environ`` dictionary in-place.
+
+    The ``os.environ`` dictionary is updated in-place so that the modification
+    is sure to work in all situations.
+
+    :param remove: Environment variables to remove.
+    :param update: Dictionary of environment variables and values to add/update.
+    """
+    env = os.environ
+    update = update or {}
+    remove = remove or []
+
+    # List of environment variables being updated or removed.
+    stomped = (set(update.keys()) | set(remove)) & set(env.keys())
+    # Environment variables and values to restore on exit.
+    update_after = {k: env[k] for k in stomped}
+    # Environment variables and values to remove on exit.
+    remove_after = frozenset(k for k in update if k not in env)
+
+    try:
+        env.update(update)
+        [env.pop(k, None) for k in remove]  # pylint: disable=expression-not-assigned
+        yield
+    finally:
+        env.update(update_after)
+        [env.pop(k) for k in remove_after]  # pylint: disable=expression-not-assigned
